eb2e5eadb7da77962fb95fff0de78073214243df
lus <lukasz.stalmach@ecedi.fr>
2023-07-05 18:09:49 +0200

    Quick fix for Google generator

--- src/Generators/Google.php
+++ src/Generators/Google.php
@@ -20,18 +20,9 @@ class Google implements Generator
     {
         $url = 'https://calendar.google.com/calendar/render?action=TEMPLATE';
 
-        $utcStartDateTime = (clone $link->from)->setTimezone(new DateTimeZone('UTC'));
-        $utcEndDateTime = (clone $link->to)->setTimezone(new DateTimeZone('UTC'));
         $dateTimeFormat = $link->allDay ? $this->dateFormat : $this->dateTimeFormat;
-        $url .= '&dates='.$utcStartDateTime->format($dateTimeFormat).'/'.$utcEndDateTime->format($dateTimeFormat);
-        
-        // Add timezone name if it is specified in both from and to dates and is the same for both
-        if (
-            $link->from->getTimezone() && $link->to->getTimezone()
-            && $link->from->getTimezone()->getName() === $link->to->getTimezone()->getName()
-        ) {
-            $url .= '&ctz=' . $link->from->getTimezone()->getName();
-        }
+        $url .= '&dates='.$link->from->format($dateTimeFormat).'/'.$link->to->format($dateTimeFormat);
+        $url .= '&ctz='.$link->from->getTimezone()->getName();
 
         $url .= '&text='.urlencode($link->title);
 
